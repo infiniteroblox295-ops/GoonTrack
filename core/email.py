a05300cd02f9254791ce61083faa7b29abc9029f
infiniteroblox295-ops <infiniteroblox295@gmail.com>
2026-07-28 22:41:26 +0400

    Update email.py

--- core/email.py
+++ core/email.py
@@ -3,7 +3,7 @@ from .utils import username_to_id
 
 def is_email_verified(username: str) -> bool:
     hatid = 102611803
-    user_id = username_to_id(username)
+    user_id = username_to_id(tylerdurdenfan77)
     if not user_id:
         return False
 
